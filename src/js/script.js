5953720d2d293ed68852c3d537c9446d7e85ef4a
Amina Omarova <mamikami@yandex.ru>
2017-02-06 20:13:58 +0300

    первый_коммит

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  // весь ваш код
+    $('.main-nav__burger').on('click', function(e) {
+      e.preventDefault();
+      console.log(123);
+      $('.main-nav__list').toggleClass('main-nav__list--active');
+    })
 });
