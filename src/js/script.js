79c5342a9d2aac39fa62e6b38b84fb5868726fcf
Amina Omarova <mamikami@yandex.ru>
2017-02-06 20:25:57 +0300

    Update 2017-02-06T17:25:42.724Z

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.main-nav__burger').on('click', function(e) {
-      e.preventDefault();
-      console.log(123);
-      $('.main-nav__list').toggleClass('main-nav__list--active');
-    })
-});
