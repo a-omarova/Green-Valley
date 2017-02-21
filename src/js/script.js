fb185b1745083c12a660f4c0dc3d124521a0e010
Amina Omarova <mamikami@yandex.ru>
2017-02-21 11:30:02 +0300

    tabs index.html

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,41 @@ $(document).ready(function(){
       $('.main-nav__list').toggleClass('main-nav__list--active');
     });
   
-    $('.promo-type__type').on('click', function(e) {
+  
+  var tabs = document.querySelectorAll('.holidays-type__type');
+  var content = document.querySelectorAll('.healthy-holidays__inner');
+  
+  for (var i=0; i<tabs.length; i++) {
+    tabs[i].addEventListener('click', function (e) {
       e.preventDefault();
-      $('.promo-type__type').toggleClass('promo-type__type--active');
-    })
+      var number = this.dataset.number;
+      document.querySelector('.holidays-type__type.holidays-type__type--active').classList.remove('holidays-type__type--active');
+      this.classList.add('holidays-type__type--active');
+      document.querySelector('.healthy-holidays__inner.healthy-holidays__inner--active').classList.remove('healthy-holidays__inner--active');
+      content[number-1].classList.add('healthy-holidays__inner--active');
+    });
+  }
+  
+    $('.photo__main-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.photo__slider'
+    });
+    $('.photo__slider').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      centerPadding: '100px',
+      asNavFor: '.photo__main-slider',
+      // dots: true,
+      centerMode: true,
+      arrows: false,
+      focusOnSelect: true
+    });
+  
+    $('.reviews-slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 2
+    });
 });
